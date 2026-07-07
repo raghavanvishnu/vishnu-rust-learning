d3c794151239817ba5c8e5e021c5b39514fd07f7
raghavanvishnu <raghavan.vmvs@gmail.com>
2026-07-07 05:44:15 +0530

    Exercise for 7 July

--- src/bin/third_script.rs
+++ src/bin/third_script.rs
@@ -1,14 +1,12 @@
 fn main() {
-    /// assigns name salary and age values
+    // assigns name salary and age values
     let name = "Vishnu";
     let age = 43;
     let salary = 25.5;
 
-    ///prints name salary and values
+    //prints name salary and values
     println!("{}", name);
     println!("{}", age);
     println!("{}", salary);
     println!("Helo scrip3");// the last line i use a s a pplaceholder
-
-
 }
